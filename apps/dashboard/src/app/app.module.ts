7cf3c43e30f9fdeb9df49c870e125558536de8c1
Vlad <vlad.betcun@gmail.com>
2021-06-17 07:17:42 +0300

    feat: create basic structure of a project

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,12 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { BooksComponent } from './books/books.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    BookDetailsComponent,
+    BooksComponent,
+    BooksListComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    RoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
